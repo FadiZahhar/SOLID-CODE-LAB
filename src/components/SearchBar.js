aec92677d32be3c79aee3da8312daca52bc9b706
Fadi Nicolas Zahhar <fnzahhar@gmail.com>
2020-02-23 00:11:42 +0200

    adding change and submit events using the arrow method

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,11 +6,25 @@ class SearchBar extends React.Component {
     state = {
         searchTerm: '',
     }
+
+    /* using the arrow function instead of binding the change method
+    * so the (this) will refer to the class and not this function. */
+
+    handleChange = (event) => {
+       this.setState({ searchTerm: event.target.value })
+    }
+
+    /* fetch our search term on the state */
+    handleSubmit = () => {
+        // using destrcuture of ES6 less code repetetion
+        const {searchTerm } = this.state;
+    }
+
     render() {
         return(
             <Paper elevation={6} style={{ padding: '25px'}}>
-              <form>
-                  <TextField fullWidth label="Search..."></TextField>
+              <form onSubmit={this.handleSubmit}>
+                  <TextField fullWidth label="Search..." onChange={this.handleChange} />
               </form>
             </Paper>
         )
